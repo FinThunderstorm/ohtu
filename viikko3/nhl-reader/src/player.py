a717998edfccb7ea9db8d0cb212d4de415c6a27f
Tuomas Alanen <tuomas@paaryna.fi>
2021-11-15 09:11:05 +0200

    t1

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, team, games, goals, assists, penalties):
         self.name = name
-    
+        self.nationality = nationality
+        self.team = team
+        self.games = games
+        self.goals = goals
+        self.assists = assists
+        self.penalties = penalties
+
     def __str__(self):
-        return self.name
+        return f'{self.name} ({self.nationality})\n - Team: {self.team}\n - Games: {self.games}\n - Goals: {self.goals}\n - Assists: {self.assists}\n - Penalties: {self.penalties} minutes\n'
