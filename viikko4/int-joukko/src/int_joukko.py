74b1b23a8f93ef2bb3b0885ed376acae356e0dd6
Tuomas Alanen <tuomas@paaryna.fi>
2021-11-22 19:04:00 +0200

    6 kuuluu metodin refaktorointi

--- viikko4/int-joukko/src/int_joukko.py
+++ viikko4/int-joukko/src/int_joukko.py
@@ -4,26 +4,13 @@ OLETUSKASVATUS = 5
 
 class IntJoukko:
     def __init__(self, kapasiteetti=KAPASITEETTI, kasvatuskoko=OLETUSKASVATUS):
-
         self.kapasiteetti = kapasiteetti
-
         self.kasvatuskoko = kasvatuskoko
-
         self.ljono = [0] * self.kapasiteetti
-
         self.alkioiden_lkm = 0
 
     def kuuluu(self, n):
-        on = 0
-
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                on = on + 1
-
-        if on > 0:
-            return True
-        else:
-            return False
+        return n in self.ljono
 
     def lisaa(self, n):
         ei_ole = 0
